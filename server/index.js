3a74665fd7a4ec22fdc2592a77ab4d6d7583e93f
DhHiep <duonghoanghiep.9b@gmail.com>
2023-04-27 23:58:25 +0700

    update middleware

--- server/index.js
+++ server/index.js
@@ -1,28 +1,34 @@
-require('dotenv').config()
-const express = require('express');
-const mongoose = require('mongoose');
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
 
-const authRouter = require("./routes/auth")
+const authRouter = require("./routes/auth");
+const cartRouter = require("./routes/cart");
 
 const connectDB = async () => {
-    try {
-        await mongoose.connect(`mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@hiepduongg.zqnjxrf.mongodb.net/digiticket?retryWrites=true&w=majority`, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-        });
-        console.log('MongoDB Connected');
-    } catch (err) {
-        console.error(err.message);
-        process.exit(1);
-    }
-}
+  try {
+    await mongoose.connect(
+      `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@hiepduongg.zqnjxrf.mongodb.net/digiticket?retryWrites=true&w=majority`,
+      {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      }
+    );
+    console.log("MongoDB Connected");
+  } catch (err) {
+    console.error(err.message);
+    process.exit(1);
+  }
+};
 
-connectDB()
+connectDB();
 
-const app = express()
-app.use(express.json())
-app.use("/api/auth", authRouter)
+const app = express();
+
+app.use(express.json());
+app.use("/api/auth", authRouter);
+app.use("/api/carts", cartRouter);
 
 const PORT = 5000;
 
-app.listen(PORT, () => console.log(`Server started on popprt ${PORT}`));
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
--- server/routes/cart.js
+++ server/routes/cart.js
@@ -1,6 +1,34 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
+const verifyToken = require("../middleware/auth");
 
-const Post = require('../models/Cart')
+const Cart = require("../models/Cart");
 
-router.post('/', asynchronous)
+router.post("/", verifyToken, async (req, res) => {
+  const { title, description, url, status } = req.body;
+
+  if (!title) {
+    return res
+      .status(400)
+      .json({ success: false, message: "Title is required" });
+  }
+
+  try {
+    const newCart = new Cart({
+      title,
+      description,
+      url: url.startsWith("https://") ? url : `https://${url}`,
+      status: status || "overdated",
+      user: req.userId,
+    });
+
+    await newCart.save();
+
+    res.json({ success: true, message: "This is your cart!", cart: newCart });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ success: false, message: "Internal server " });
+  }
+});
+
+module.exports = router;
