0607360c8be31cae09496f7997a0f209cc1fe976
DuongHiepp <hiepdh@icebear.com.vn>
2023-04-27 18:13:42 +0700

    update server

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -29,9 +29,49 @@ router.post("/register", async (req, res) => {
       process.env.ACCESS_TOKEN
     );
 
-    res.json({ success: true, message: "user created successfully" }, accessToken);
+    res.json({
+      success: true,
+      message: "user created successfully",
+      accessToken,
+    });
   } catch (err) {
-    throw err;
+    res.status(500).json({ success: false, message: "Internal server error" });
+  }
+});
+
+router.post("/login", async (req, res) => {
+  const { username, password } = req.body;
+
+  if (!username || !password) {
+    return res.status(400).json({
+      success: false,
+      message: "Missing username or password",
+    });
+  }
+
+  try {
+    const user = await User.findOne({ username: username });
+    if (!user) {
+      return res.status(400).json({
+        success: false,
+        message: "Incorrect username or password",
+      });
+    } else if (user.password !== password) {
+      return res.status(400).json({
+        success: false,
+        message: "Incorrect username or password",
+      });
+    }
+
+    const accessToken = jwt.sign(
+      { userID: user._id },
+      process.env.ACCESS_TOKEN
+    );
+
+    res.json({ success: true, message: "User login Success", accessToken });
+
+  } catch (err) {
+    res.status(500).json({ success: false, message: "Internal server error" });
   }
 });
 
